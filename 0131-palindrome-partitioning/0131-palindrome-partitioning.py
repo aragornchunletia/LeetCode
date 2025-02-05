838397d29d0657ad54743bfb2037c2c40e79f0ef
aragornchunletia <42561076+aragornchunletia@users.noreply.github.com>
2025-02-05 16:26:54 +0530

    Time: 49 ms (61.69%), Space: 34.5 MB (67.06%) - LeetHub

---
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.py
@@ -0,0 +1,28 @@
+class Solution:
+    def partition(self, s: str) -> List[List[str]]:
+        
+        def isPalindrome(start , end):
+            while start <= end:
+                if s[start] != s[end]:  return False
+                start , end = start+1 ,end-1
+            return True
+
+
+        def helper(sIdx , part):
+            if sIdx == N:
+                res.append(part[:])
+                return
+
+            for i in range(sIdx , N):
+                if isPalindrome(sIdx , i):
+                    part.append(s[sIdx:i+1])
+                    helper(i+1 , part)
+                    part.pop()
+
+
+        N = len(s)
+        res = []
+        helper(0,[])
+        return res
+
+
