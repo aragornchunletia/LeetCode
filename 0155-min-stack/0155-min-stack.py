80b64c85bfaf0eb155f5891426f2b17938cc38a8
aragornchunletia <42561076+aragornchunletia@users.noreply.github.com>
2025-02-05 18:00:42 +0530

    Time: 11 ms (25.36%), Space: 21.5 MB (29.74%) - LeetHub

---
+++ 0155-min-stack/0155-min-stack.py
@@ -0,0 +1,29 @@
+from collections import deque
+class MinStack:
+
+    def __init__(self):
+        self.stack = deque([])
+        self.min_stack = deque([])
+        
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        self.min_stack.append(min(val , self.min_stack[-1] if self.min_stack else val))
+
+    def pop(self) -> None:
+        val = self.stack.pop()
+        self.min_stack.pop()
+        return val
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        return self.min_stack[-1]
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(val)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
